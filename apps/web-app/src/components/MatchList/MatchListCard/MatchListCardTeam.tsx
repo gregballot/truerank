8b59f353df04a0fd172e93e7d6bd5fee6627f785
Greg <gregballot@users.noreply.github.com>
2025-04-08 02:07:11 +0200

    Team section in matchlist card done with style + links

--- apps/web-app/src/components/MatchList/MatchListCard/MatchListCardTeam.tsx
+++ apps/web-app/src/components/MatchList/MatchListCard/MatchListCardTeam.tsx
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom";
+
 import { SharedTypes } from "@truerank/shared";
 
 import styles from "./styles/MatchListCardTeam.module.css";
+import { getChampionIcon } from "../../../helpers/datadragon";
 
 type Props = {
   team: SharedTypes.MatchParticipant[];
@@ -12,9 +15,29 @@ export function MatchListCardTeam({
   return (
     <ul className={styles.matchListCardTeam}>
       {
-        team.map((player, index) => (
-          <li key={index}>{ player.summoner.gameName }</li>
-        ))
+        team.map(({ summoner, championName}, index) => {
+          const summonerName = summoner.gameName;
+          const summonerTag = summoner.tagLine;
+          const playerLink = `/player/${summonerName}/${summonerTag}`;
+
+          return (
+            <li key={index}>
+              <img
+                src={ getChampionIcon(championName) }
+                alt={ championName }
+                title={ championName } />
+              <p>
+                <Link
+                  to={playerLink}
+                  className={styles.playerLink}
+                  title={summonerName}
+                >
+                  { summoner.gameName }
+                </Link>
+              </p>
+            </li>
+          );
+        })
       }
     </ul>
   );
