fbec193581f539287f0b56c0af75f6cb16eb986d
Greg <gregballot@users.noreply.github.com>
2025-04-13 02:57:23 +0200

    overall style improvements, sidebar improvements

--- apps/web-app/src/components/SummonerProfile/ProfileSidebar/ProfileSidebarRanking.tsx
+++ apps/web-app/src/components/SummonerProfile/ProfileSidebar/ProfileSidebarRanking.tsx
@@ -20,20 +20,45 @@ export function ProfileSidebarRanking(
       {
         ranking ? (
           <>
-            <h3>{ name }</h3>
-            <img
-              src={getCustomEmblemUrl(ranking.rank)}
-              alt={ranking.rank}
-            />
-            <h4>{ ranking.rank }{ ranking.rank !== "CHALLENGER" && ` ${ranking.division.formatted}` }</h4>
-            { ranking.lpAmount } LP<br/>
+            <h3 className={styles.queueName}>{ name }</h3>
+            <div className={styles.rankSection}>
+              <img
+                className={styles.emblem}
+                src={getCustomEmblemUrl(ranking.rank)}
+                alt={ranking.rank}
+              />
+              <div className={styles.rankingInfo}>
+                <h4 className={styles.rankName}>
+                  { ranking.rank }
+                  { ranking.rank !== "CHALLENGER" && ` ${ranking.division.formatted}` }
+                </h4>
+                <p className={styles.lp}>
+                  { ranking.lpAmount } LP<br/>
+                </p>
+              </div>
+            </div>
 
-            { ranking.wins }W / { ranking.losses }L<br/>
-            <strong>Winrate:</strong> { winRate?.toFixed(1) }%<br/>
+            <div className={styles.rankStats}>
+              <div className={styles.rankBar}>
+                <div
+                  className={styles.rankBarWin}
+                  style={{ flexBasis: `${winRate}%` }}
+                />
+                <div
+                  className={styles.rankBarLose}
+                  style={{ flexBasis: `${100 - winRate!}%` }}
+                />
+              </div>
+              <div className={styles.rankBarLabel}>
+                <span>{ranking.wins}W</span>
+                <span className={styles.rankBarWinrate}>Winrate: {winRate!.toFixed(1)}%</span>
+                <span>{ranking.losses}L</span>
+              </div>
+            </div>
           </>
         ) : (
           <>
-            <h3>{ name }: Unranked</h3>
+            <h3 className={styles.queueName}>{ name }: Unranked</h3>
           </>
         )
       }
